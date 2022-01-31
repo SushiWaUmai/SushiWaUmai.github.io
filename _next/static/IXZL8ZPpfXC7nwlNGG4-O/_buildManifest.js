6e7a295c5d86d8aa9ed996e82f2ee22df4fcb33a
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2022-01-31 21:40:02 +0000

    Deploying to gh-pages from @ SushiWaUmai/SushiWaUmai.github.io@56c14aaf14c3323c78d40b880d7a249bfa687807 🚀

---
+++ _next/static/IXZL8ZPpfXC7nwlNGG4-O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-f714215d5407f831.js","static/chunks/0c428ae2-a17f9e6fc8d10b82.js","static/chunks/pages/index-f52be9041863fbac.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

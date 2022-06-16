5a9112240ac5ff9fea598639bc85c6a46f3d7e2c
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-16 23:04:51 +0200

    feat: :sparkles: add twitter to social media

--- src/components/socialmedia.tsx
+++ src/components/socialmedia.tsx
@@ -4,11 +4,12 @@ import {
   FaLinkedin,
   FaItchIo,
   FaDiscord,
+  FaTwitter,
 } from "react-icons/fa";
 
-interface IntroProps {}
+interface SocialMediaProps {}
 
-export const SocialMediaComponent: React.FC<IntroProps> = () => {
+export const SocialMediaComponent: React.FC<SocialMediaProps> = () => {
   return (
     <>
       <a href="https://github.com/SushiWaUmai" className="btn">
@@ -20,14 +21,17 @@ export const SocialMediaComponent: React.FC<IntroProps> = () => {
       <a href="https://www.instagram.com/eugenematsumura/" className="btn">
         <FaInstagram size={32} />
       </a>
+      <a href="https://discord.com/users/356155984412147723/" className="btn">
+        <FaDiscord size={32} />
+      </a>
+      <a href="https://twitter.com/EugeneMatsumur1" className="btn">
+        <FaTwitter size={32} />
+      </a>
       <a
         href="https://www.linkedin.com/in/eugene-matsumura-68334922b/"
         className="btn"
       >
         <FaLinkedin size={32} />
-      </a>
-      <a href="https://discord.com/users/356155984412147723/" className="btn">
-        <FaDiscord size={32} />
       </a>
     </>
   );
